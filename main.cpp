0b6c175a75484386b3b8a49cee5187c3f50bbeda
PaMeirelles <rafaelpmeirelles@.com>
2023-06-01 21:45:02 -0300

    Valid hashes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 #include "hash.h"
 #include "board.h"
 
@@ -7,24 +8,29 @@ int main() {
     int invalid = 0;
     int redundant = 0;
     int valid = 0;
-    for(int hash = minHash; hash < maxHash; hash++){
-        if(!validateHash(hash)){
+
+    std::ofstream outputFile("valid_hashes.txt"); // Open the file for writing
+
+    for (int hash = minHash; hash < maxHash; hash++) {
+        if (!validateHash(hash)) {
             invalid++;
-        }
-        else{
-            if(reduceHash(hash) == hash){
+        } else {
+            if (reduceHash(hash) == hash) {
                 valid++;
-            }
-            else{
+                outputFile << hash << std::endl; // Write valid hash to the file
+            } else {
                 redundant++;
             }
         }
-
     }
+
+    // Close the file after writing
+    outputFile.close();
+
     // Print the final results
-std::cout << "Invalid: " << invalid << " (" << std::fixed << std::setprecision(2) << (invalid * 100.0 / maxHash) << "%)" << std::endl;
-std::cout << "Redundant: " << redundant << " (" << std::fixed << std::setprecision(2) << (redundant * 100.0 / maxHash) << "%)" << std::endl;
-std::cout << "Valid: " << valid << " (" << std::fixed << std::setprecision(2) << (valid * 100.0 / maxHash) << "%)" << std::endl;
- 
+    std::cout << "Invalid: " << invalid << " (" << std::fixed << std::setprecision(2) << (invalid * 100.0 / maxHash) << "%)" << std::endl;
+    std::cout << "Redundant: " << redundant << " (" << std::fixed << std::setprecision(2) << (redundant * 100.0 / maxHash) << "%)" << std::endl;
+    std::cout << "Valid: " << valid << " (" << std::fixed << std::setprecision(2) << (valid * 100.0 / maxHash) << "%)" << std::endl;
+
     return 0;
 }
