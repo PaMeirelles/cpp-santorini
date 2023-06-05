a59b9d7d0494c3220273fe22c82d66a98fd49f00
Replit user <>
2023-06-05 13:15:30 +0000

    Engine Assembly

--- matchManager.cpp
+++ matchManager.cpp
@@ -1,67 +1,82 @@
 #include "matchManager.h"
+#include "eval.h"
+#include "search.h"
+#include "timeManagement.h"
 
-int evalPosition(std::function<int(Board)> eval, int hash){
-    int w[4];
-    unhashWorkers(hash, w);
-    Board board = Board(w);
-    return eval(board);
+int evalPosition(std::function<int(Board)> eval, int hash) {
+  int w[4];
+  unhashWorkers(hash, w);
+  Board board = Board(w);
+  return eval(board);
 }
 
+std::set<int> getPositionsForMatch(std::function<int(Board)> evalOne,
+                                   std::function<int(Board)> evalTwo, int n) {
+  std::ifstream inputFile("valid_hashes.txt");
+  std::vector<int> hashListOne;
 
-std::set<int> getPositionsForMatch(std::function<int(Board)> evalOne, std::function<int(Board)> evalTwo, int n){
-    std::ifstream inputFile("valid_hashes.txt");
-    std::vector<int> hashListOne;
+  if (inputFile.is_open()) {
+    std::string line;
+    while (std::getline(inputFile, line)) {
+      try {
+        int hash = std::stoi(line);
+        hashListOne.push_back(hash);
+      } catch (const std::exception &e) {
+        // Handle invalid lines if needed
+        std::cout << "Invalid line: " << line << std::endl;
+      }
+    }
+    inputFile.close();
 
-    if (inputFile.is_open()) {
-        std::string line;
-        while (std::getline(inputFile, line)) {
-            try {
-                int hash = std::stoi(line);
-                hashListOne.push_back(hash);
-            } catch (const std::exception& e) {
-                // Handle invalid lines if needed
-                std::cout << "Invalid line: " << line << std::endl;
-            }
-        }
-        inputFile.close();
+  } else {
+    std::cout << "Failed to open the file." << std::endl;
+  }
 
+  std::vector<int> hashListTwo(hashListOne);
+
+  std::sort(hashListOne.begin(), hashListOne.end(), [&](int a, int b) {
+    return evalPosition(evalOne, a) < evalPosition(evalOne, b);
+  });
+  std::sort(hashListTwo.begin(), hashListTwo.end(), [&](int a, int b) {
+    return evalPosition(evalTwo, a) < evalPosition(evalTwo, b);
+  });
+
+  std::set<int> setHash(hashListOne.begin(), hashListOne.end());
+
+  int indexOne = 0;
+  int indexTwo = hashListTwo.size() - 1;
+  int current = 1;
+  std::set<int>::iterator it;
+  while (setHash.size() > n) {
+    if (current == 1) {
+      it = setHash.find(hashListOne[indexOne]);
+      indexOne++;
+      if (it != setHash.end()) {
+        setHash.erase(it);
+        current = 2;
+      }
     } else {
-        std::cout << "Failed to open the file." << std::endl;
+      it = setHash.find(hashListTwo[indexTwo]);
+      indexTwo--;
+      if (it != setHash.end()) {
+        setHash.erase(it);
+        current = 1;
+      }
     }
-
-    std::vector<int> hashListTwo(hashListOne);
-
-    std::sort(hashListOne.begin(), hashListOne.end(), [&](int a, int b) {
-        return evalPosition(evalOne, a) < evalPosition(evalOne, b);
-    });
-    std::sort(hashListTwo.begin(), hashListTwo.end(), [&](int a, int b) {
-        return evalPosition(evalTwo, a) < evalPosition(evalTwo, b);
-    });
-
-
-    std::set<int> setHash(hashListOne.begin(), hashListOne.end());
-
-    int indexOne = 0;
-    int indexTwo = hashListTwo.size() - 1;
-    int current = 1;
-    std::set<int>::iterator it;
-    while(setHash.size() > n){
-        if(current == 1){
-            it = setHash.find(hashListOne[indexOne]);
-            indexOne++;
-            if(it != setHash.end()){
-                setHash.erase(it);
-                current = 2;            
-            }
-        }
-        else{
-            it = setHash.find(hashListTwo[indexTwo]);
-            indexTwo--;
-            if(it != setHash.end()){
-                setHash.erase(it);
-                current = 1;
-            }
-        }
-    }
-    return setHash;
+  }
+  return setHash;
+}
+EngineInfo assemblyEngine(std::string name) {
+  EngineInfo eInfo;
+  if (name == "Titan") {
+    eInfo.search = negamax;
+    eInfo.eval = nh_s;
+    eInfo.timeManager = et_s;
+  } else if (name == "Ruler") {
+    eInfo.search = negamax;
+    eInfo.eval = nh_c;
+    eInfo.timeManager = et_s;
+  } else {
+    throw std::runtime_error("Invalid engine");
+  }
 }
--- matchManager.h
+++ matchManager.h
@@ -1,18 +1,29 @@
 #ifndef MATCH_MANAGER_H
 #define MATCH_MANAGER_H
 
-#include <functional>
 #include "board.h"
 #include "hash.h"
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
 #include <set>
+#include <string>
+#include <vector>
 
 int evalPosition(std::function<int(Board)> eval, int hash);
 
-std::set<int> getPositionsForMatch(std::function<int(Board)> evalOne, std::function<int(Board)> evalTwo, int n);
+std::set<int> getPositionsForMatch(std::function<int(Board)> evalOne,
+                                   std::function<int(Board)> evalTwo, int n);
+
+struct EngineInfo {
+  std::function<int(Board, int, std::function<int(Board)>)> search;
+  std::function<int(Board)> eval;
+  std::function<int(int)> timeManager;
+};
+
+enum Engines { Titan, Ruler };
+void playMatch(int startingPos, int time, std::string playerA,
+               std::string playerB);
 
 #endif /* MATCH_MANAGER_H */
