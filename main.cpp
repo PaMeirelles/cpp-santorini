ccb8a1ca5bd07ff2d14400e0e8a38d71e214df7f
PaMeirelles <rafaelpmeirelles@.com>
2023-06-07 18:24:44 -0300

    Benchmarks

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "match.h"
+#include "benchmark.h"
 
 int main() { 
-    play(1000 * 60 * 1, 12, "Aegis", "Ascendant");
+    // testUpTo("negamax", "alphabeta", 4, 8);
+    play(1000 * 60 * 15, 1, "Ascendant", "Whisper");
  }
