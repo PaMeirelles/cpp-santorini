306e6bb502aaf27fcb5b7288ebe1128d6a82bda9
PaMeirelles <rafaelpmeirelles@.com>
2023-06-10 08:33:02 -0300

    Whisper games

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "postMatch.h"
 
 int main() { 
-    play(1 * 60 * 1000, 6, "Ascendant", "Whisper");
+    play(1 * 60 * 1000, 120, "Ascendant", "Whisper");
     //  testUpTo("climb mo v10", "alphabeta", 10, 6);
 
  }
