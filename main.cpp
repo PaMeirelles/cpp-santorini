11ca7b15d41c72a535d0e01970e4c79e755ce070
PaMeirelles <rafaelpmeirelles@.com>
2023-06-11 14:10:32 -0300

    Ascendant e Whisper ainda empatados

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 #include "hashTable.h"
 
 int main() { 
-    // play(1 * 60 * 1000, 1200, "Ascendant", "Whisper");
+    play(1 * 60 * 1000, 1200, "Ascendant", "Whisper");
     //  testUpTo("climb mo v10", "alphabeta", 10, 6);
-    Board b = Board();
-    HashTable ht;
-    allocateHashTable(&ht, 400);
+    // Board b = Board();
+    // HashTable ht;
+    // allocateHashTable(&ht, 400);
  }
