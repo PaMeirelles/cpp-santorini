621752c553ee5e14754eb8e67773ec96f4906f42
PaMeirelles <rafaelpmeirelles@.com>
2023-06-12 16:38:01 -0300

    More games

--- main.cpp
+++ main.cpp
@@ -4,11 +4,9 @@
 #include "hashTable.h"
 
 int main() { 
-    // play(1 * 60 * 1000, 12, "Titan", "Fisher");
-    //  testUpTo("climb mo v10", "alphabeta", 10, 6);
-    // Board b = Board();
-    // HashTable ht;
-    // allocateHashTable(&ht, 400);
-    heal(60 * 1000 * 1);
+    while(true){
+        play(1000 * 60 * 1, 12, "Ascendant", "Whisper");
+        heal(60 * 1000 * 1);
 
+    }
  }
