c96bf9e3f6112fbec051ce80e2a90d3b192830f4
PaMeirelles <rafaelpmeirelles@.com>
2023-06-17 11:16:27 -0300

    Saving games

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main() { 
     while (true){
-        system("C:/Users/Usuário/AppData/Local/Microsoft/WindowsApps/python3.11.exe c:/Users/Usuário/santorini/core.py");
+        system("python core.py");
         system(".\\pushToGit.bat");
         play(1000 * 60 * 1, 12, "Ascendant", "Cosmic");
     }
