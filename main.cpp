ccb9128f08ffd93721d48ac71c8dd301ed0affcf
PaMeirelles <rafaelpmeirelles@.com>
2023-06-21 20:54:30 -0300

    Saving games

--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@
 using namespace std;
 
 int main() { 
-    play(1000 * 60 * 30, 1, "Cosmic", "Vortex");
-    // int defaultTime = 1000 * 60;
-    // play(defaultTime, 12, "Cosmic", "Angel");
-    // while (true){
-    //     system("python core.py");
-    //     system(".\\pushToGit.bat");
-    //     heal(defaultTime);
-    //     playHighestConfidence(defaultTime, 12);
-    // }
+    // play(1000 * 60 * 30, 1, "Cosmic", "Vortex");
+    int defaultTime = 1000 * 60;
+    play(defaultTime, 12, "Cosmic", "Vortex");
+    while (true){
+        system("python core.py");
+        system(".\\pushToGit.bat");
+        heal(defaultTime);
+        playHighestConfidence(defaultTime, 12);
+    }
     // int w[4] = {0, 12, 1, 2};
     // Board b = Board(w);
     // std::cout << nh_s(b) << std::endl;
