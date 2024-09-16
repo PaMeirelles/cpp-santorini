c0f883ab04a2537f1604bc83e4b9d69a0cd1b9f5
PaMeirelles <rafaelpmeirelles@gmail.com>
2024-09-16 10:04:05 -0300

    Polishing board

--- board.h
+++ board.h
@@ -1,28 +1,29 @@
+#pragma once
 #include "move.h"
 
 #ifndef BOARD_H
 #define BOARD_H
 
 struct Board {
-    int squares[25];
-    int workers[4];
+    int squares[25]{};
+    int workers[4]{};
     int turn;
     int ply;
 
-    Board(int);
+    explicit Board(int);
     Board(const int (&w)[4], const int (&s)[25]);
-    Board(const int (&w)[4]);
+    explicit Board(const int (&w)[4]);
     Board();
 
     void print() const;
-    int getWorkerHeight (int workerId);
-    int getWorkerPos (int workerId);
-    void makeMove (Move move);
-    void unmakeMove (Move move);
+    int getWorkerHeight (int workerId) const;
+    int getWorkerPos (int workerId) const;
+    void makeMove (const Move &move);
+    void unmakeMove (const Move &move);
     std::vector<Move> gen_moves(int player);
-    std::vector<Move> gen_half_moves(int player);
-    bool isFree(int square);
-    int getHeight(int square);
+    std::vector<Move> gen_half_moves(int player) const;
+    bool isFree(int square) const;
+    int getHeight(int square) const;
 
 };
 #endif
